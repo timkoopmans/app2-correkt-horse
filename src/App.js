262b901742edce41918deae50e1ace62b2ef2a89
Tim Koopmans <tim.koops@gmail.com>
2023-08-22 20:52:03 +1000

    use cookies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import './App.css';
 import {useAuth} from "@frontegg/react";
+import React, { useState, useEffect } from 'react';
 
 function App() {
+    const [setCookieValue] = useState(null);
+
+    useEffect(() => {
+        // On component mount, read the cookie value
+        const value = getCookie("loginOrigin");
+        setCookieValue(value);
+    }, []);
+    const setCookie = (name, value, days) => {
+        let expires = "";
+        if (days) {
+            const date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "") + expires + "; domain=.correkt.horse; path=/";
+    };
+
+    const getCookie = (name) => {
+        const nameEQ = name + "=";
+        const ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    };
+
     const {user, isAuthenticated} = useAuth();
 
     const logout = () => {
@@ -9,6 +38,7 @@ function App() {
     }
 
     const redirectToLogin = () => {
+        setCookie("loginOrigin", "https://app2.correkt.horse", 7);
         window.location.href = `https://auth.correkt.horse?redirectUrl=${window.location}`;
     }
 
